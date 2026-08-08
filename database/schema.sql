cc9a9f9f36b4fdb9c3d970748f12a86101ddccea
Rahat Mohashin Zarif <rahatzarif0443@gmail.com>
2026-08-08 17:30:06 +0600

    created sql files

---
+++ database/schema.sql
@@ -0,0 +1,8 @@
+/*BookMyTrip/
+  database/
+    schema.sql       ← CREATE TABLE statements, all 13 tables
+    triggers.sql      ← ticket_audit_log trigger, etc.
+    functions.sql     ← stored functions
+    procedures.sql     ← cancel_event(), booking procedure
+    queries.sql        ← your 3+ complex join/aggregation queries*/
+    
